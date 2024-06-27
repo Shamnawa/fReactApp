a3d48cf7b4fc98994705af7e622d2f2799fe2cd5
shamnawa19 <shabbirkyk0@gmail.com>
2024-06-27 10:53:10 +0530

    first commit of reactApp

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  
+
+  return (
+    <>
+      
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,164 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Footer() {
+    return (
+        <footer className="bg-white border-y">
+            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+                <div className="md:flex md:justify-between">
+                    <div className="mb-6 md:mb-0">
+                        <Link to="/" className="flex items-center">
+                            <img
+                                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                                className="mr-3 h-16"
+                                alt="Logo"
+                            />
+                        </Link>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
+                                    <Link to="/" className="hover:underline">
+                                        Home
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/about" className="hover:underline">
+                                        About
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
+                                    <a
+                                        href="https://github.com/Shamnawa"
+                                        className="hover:underline"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Github
+                                    </a>
+                                </li>
+                                <li>
+                                    <Link to="/" className="hover:underline">
+                                        Discord
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
+                                    <Link to="#" className="hover:underline">
+                                        Privacy Policy
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="#" className="hover:underline">
+                                        Terms &amp; Conditions
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <hr className="my-6 border-red-700 sm:mx-auto lg:my-8" />
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <span className="text-sm text-green-700 sm:text-center bg-pink-200">
+                        © 2024
+                        <a href="https://mypages90843.netlify.app/" className="hover:underline">
+                            Shabbir-Np
+                        </a>
+                        . All Rights Reserved.
+                    </span>
+                    <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+                        <Link to="https://www.facebook.com/md.shabbir.9066389" className="text-gray-500 hover:text-blue-500">
+                            <svg
+                                className="w-4 h-4"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 8 19"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            <span className="sr-only">Facebook page</span>
+                        </Link>
+
+                        <Link to="#" className="text-gray-500 hover:text-red-900">
+                            <svg
+                                className="w-4 h-4"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 20 17"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            <span className="sr-only">X</span>
+                        </Link>
+                        <Link to="https://github.com/Shamnawa" className="text-gray-500 hover:text-yellow-300">
+                            <svg
+                                className="w-4 h-4"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            <span className="sr-only">GitHub account</span>
+                        </Link>
+                        <Link to='https://www.instagram.com/shabbirkyk0/' className="text-gray-500  hover:text-pink-500">
+
+                            <svg
+                                className="w-4 h-4"
+
+
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M16.98 0a6.9 6.9 0 0 1 5.08 1.98A6.94 6.94 0 0 1 24 7.02v9.96c0 2.08-.68 3.87-1.98 5.13A7.14 7.14 0 0 1 16.94 24H7.06a7.06 7.06 0 0 1-5.03-1.89A6.96 6.96 0 0 1 0 16.94V7.02C0 2.8 2.8 0 7.02 0h9.96zm.05 2.23H7.06c-1.45 0-2.7.43-3.53 1.25a4.82 4.82 0 0 0-1.3 3.54v9.92c0 1.5.43 2.7 1.3 3.58a5 5 0 0 0 3.53 1.25h9.88a5 5 0 0 0 3.53-1.25 4.73 4.73 0 0 0 1.4-3.54V7.02a5 5 0 0 0-1.3-3.49 4.82 4.82 0 0 0-3.54-1.3zM12 5.76c3.39 0 6.2 2.8 6.2 6.2a6.2 6.2 0 0 1-12.4 0 6.2 6.2 0 0 1 6.2-6.2zm0 2.22a3.99 3.99 0 0 0-3.97 3.97A3.99 3.99 0 0 0 12 15.92a3.99 3.99 0 0 0 3.97-3.97A3.99 3.99 0 0 0 12 7.98zm6.44-3.77a1.4 1.4 0 1 1 0 2.8 1.4 1.4 0 0 1 0-2.8z"
+                                   
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            <span className="sr-only">Dribbble account</span>
+
+
+
+
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+
+
+
+
